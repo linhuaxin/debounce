cb4ec340d863e11aeea267ea77c247c90728fb96
linhuaxin <linhuaxin@foxmail.com>
2021-12-03 23:07:05 +0800

    feat: add firstRun config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-function debounce(fn, delay) {
+function debounce(fn, delay, config = {
+  runFirst: false
+}) {
   let timer = null;
+  let isInvolved = false;
+  let { runFirst } = config;
 
   return function (...args) {
+    if (runFirst && !isInvolved) {
+      fn.apply(this, args);
+      isInvolved = true;
+      return;
+    }
+
     if (timer) {
       clearTimeout(timer);
     }
 
     timer = setTimeout(() => {
       fn.apply(this, args);
+      isInvolved = false;
     }, delay);
   }
 }
